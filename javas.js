7bf64c58358382db2d0bac4269694364c26b3cf8
ItryCode <sonuhrajeev@gmail.com>
2023-07-23 11:45:29 +0530

    Added a skeleton and basic game functionality

---
+++ javas.js
@@ -0,0 +1,71 @@
+const GameBoard=(()=>{
+    let gameboard=["","","","","","","","",""]
+    const render=()=>{
+        let boardHTML="";
+        gameboard.forEach((square,index)=>{
+            boardHTML+=`<div class="square" id="square-${index}">${square}</div>`
+        })
+
+        document.querySelector("#gameboard").innerHTML=boardHTML;
+
+        const squares=document.querySelectorAll(".square")
+        squares.forEach((square)=>{
+            square.addEventListener('click',Game.handleClick);
+        })
+    }  
+    
+
+    return{
+        render,
+    }
+})();
+
+
+const createPlayer=(name,mark)=>{
+    return{
+        name,
+        mark
+    }
+}
+
+const Game=(()=>{
+    let players=[];
+    let currentPlayerIndex
+    let gameOver
+
+    const start=()=>{
+        players=[
+            createPlayer(document.querySelector("#player1").value,"X"),
+            createPlayer(document.querySelector("#player2").value,"O")
+        ]
+        currentPlayerIndex=0;
+        gameOver=false;
+        GameBoard.render();
+    }
+
+    const handleClick=(event)=>{
+        let index=parseInt(event.target.id.split('-')[1]);
+        console.log(index)
+
+
+    }
+
+    return{
+        start,
+        handleClick
+    }
+})();
+
+const startButton=document.querySelector("#start-button")
+startButton.addEventListener("click",()=>{
+    Game.start();
+})
+
+// const GameBoard=(()=>{
+//     const gameBoard=[" "," "," "," "," "," "," "]
+//     return{
+//         gameBoard
+//     }
+// })();
+
+// GameBoard
